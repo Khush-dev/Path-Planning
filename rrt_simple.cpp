dbf5f7d69d6a796e62911941d120700b60b178d4
Khush Jain <76247518+Khush-dev@users.noreply.github.com>
2021-03-26 16:27:10 +0530

    Update rrt_simple.cpp

--- rrt_simple.cpp
+++ rrt_simple.cpp
@@ -10,21 +10,13 @@ using namespace std;
 #define WINDOW_X 1370
 #define WINDOW_Y 750
 
-/*struct cell{
-    pair<int, int> parent;
-    cell(): parent(-1,-1)
-    {}
-    cell(pair<int, int> parentx) {
-        parent = parentx;
-    }
-};*/
 
 double dist(pair <int, int> n1, pair <int, int> n2,int len, int br){
     return pow( pow(len*(n1.first-n2.first), 2)+ pow(br*(n1.second-n2.second), 2), 0.5 );
 }
 
 bool any_obs(pair <int, int> n1, pair <int, int> n2, vector<vector<bool>>& input, int len, int br){
-    int i, j;  cout << " any_obs between ("<< n1.first+1 <<',' << n1.second+1<< ") & (" << n2.first+1 <<',' << n2.second+1<< ')'<< endl;
+    int i, j;
     if(n2.first - n1.first == 0){
         i = n1.first;
         for(int k = 0; k<abs(n2.second - n1.second); k++){
@@ -33,19 +25,15 @@ bool any_obs(pair <int, int> n1, pair <int, int> n2, vector<vector<bool>>& input
         }
         return false;
     }
-    //double tan = double(n2.second - n1.second)/(n2.first - n1.first)*double(br)/len;
-    //double cos = pow(1+tan*tan, -0.5), sin = pow(tan*tan/(1+tan*tan), 0.5);
 
     double d = dist(n1, n2, len, br);
     double seg = d/max(abs(n2.second - n1.second), abs(n2.first - n1.first));     //distance always >=0, a line segment
     double cos = len*(n2.first - n1.first)/d, sin = br*(n2.second - n1.second)/d;
-    //i = d/(n2.first - n1.first);
-    //j  = d/(n2.second - n1.second);
-    cout << "seg: "<< seg << " sin: "<< sin<< " cos: "<< cos << " d: "<< d << endl;
+
     for(int k = 1; k<max(abs(n2.second - n1.second)+1, abs(n2.first - n1.first)+1 ); k++){
         i = n1.first + int(k*seg*cos/len);
         j = n1.second + int(k*seg*sin/br);
-        cout << "checking ("<< i+1 << ','<< j+1 << ')' << endl;
+
         if(input[i][j]) return true;
     }
     return false;
@@ -105,56 +93,67 @@ void make_grid(int n, int m, int len, int br){
 
 void rrt(vector<vector<bool>>& input, pair<int,int> src, pair<int,int> dest, int n, int m, int len, int br){
     vector<vector<pair <int, int>>> parent (n, vector<pair <int, int>> (m, make_pair(-1,-1)));      //initialised vector of parents
-    int counter = 1, N = int(n*m/15);
+    int counter = 1, N = int(n*m/10);
     vector<vector<bool>> closedlist(n, vector<bool> (m,0));
-    vector<pair <int, int>> nodes;
+    vector<vector<double>> cost (n, vector<double> (m,-1));
+    vector<pair <int, int>> nodes, neighbours;
     nodes.push_back(src);
-    int i,j; pair<int,int> node;
-    double d;//r = 3*(len + br),
-    //cout << "nodes: " << endl;
+    cost[src.first][src.second] = 0;
+    int i,j; pair<int,int> node;    //both refer to the ***same point*** i.e. current node
+    double d, r = 5*(len + br);
+    Rectangle tmp(len/2,br/2,len,br);
+    tmp.imprint();
     while(counter < N){
         i = randuv(1,n-1); j = randuv(1,m-1);
         node = make_pair(i,j);  //current node
         if(closedlist[i][j]) continue;
         if(input[i][j]) continue;
-        d = INF;
+        d = INF; neighbours.clear();
         for(auto n:nodes){      //EXTEND ()
             if(dist(n, node, len, br) > d)continue;
             if(any_obs(n, node, input, len, br)) continue;
             parent[i][j] = n;       //nearest neighbour is the parent
 
             closedlist[i][j] = true;
-            d = dist(n, node, len, br);
-            //cout << " dist between ("<< n.first+1 <<',' << n.second+1<< ") & (" << node.first+1 <<',' << node.second+1<< ")  :" << d << endl;;
+            cost[i][j] = d + cost[n.first][n.second];
         }
-        if(closedlist[i][j]){
-            imprintLine(i*len + len/2, j*br+len/2, parent[i][j].first*len + len/2, parent[i][j].second*br + br/2);
-            nodes.push_back(node);
-            counter++;
-            //cout << " (" << node.first+1 <<',' << node.second+1 << ") ";
-            //if(!(dist(node, dest, len, br)>r))
-                if(!any_obs(node, dest, input, len, br)){
-                    parent[dest.first][dest.second] = node;
-                    imprintLine(node.first*len + len/2, node.second*br + br/2, dest.first*len + len/2, dest.second*br + br/2);
-                    nodes.push_back(dest); closedlist[dest.first][dest.second] = true;
-                    break;
-                }
-        }
-    }/*
-    for(auto arr:closedlist)
-        for(auto bl:arr){
-            cout << bl<< " ";
-        }cout << endl;
+        if(!closedlist[i][j]) continue;
 
-    for(auto arr:parent)
-        for(auto bo:arr){
-            cout << " ("<< bo.first+1<< ',' << bo.second+1 << ") ";
-        }cout <<" --- " << endl;*/
+        for(auto n:nodes){      //updating neighbours
+            if(any_obs(n, node, input, len, br)) continue;
+            d = dist(n, node, len, br);             //reusing variable d
+            if(d > r)continue;
+            neighbours.push_back(n);
+
+            if(cost[i][j] > cost[n.first][n.second] + d){
+              parent[i][j] = n;
+              closedlist[i][j] = true;
+              cost[i][j] = d + cost[n.first][n.second];
+            }
+        }
+        imprintLine(i*len + len/2, j*br+len/2, parent[i][j].first*len + len/2, parent[i][j].second*br + br/2);
+
+        for(auto n:neighbours){
+          d = dist(n, node, len, br);             //reusing variable d
+          if(cost[i][j] + d < cost[n.first][n.second]){
+            parent[n.first][n.second] = node;
+            cost[n.first][n.second] = cost[i][j] + d;
+            imprintLine(i*len + len/2, j*br+len/2, n.first*len + len/2, n.second*br + br/2);
+          }
+        }
+        nodes.push_back(node);
+        counter++;
+        if(!any_obs(node, dest, input, len, br)){
+             parent[dest.first][dest.second] = node;
+             imprintLine(node.first*len + len/2, node.second*br + br/2, dest.first*len + len/2, dest.second*br + br/2);
+             nodes.push_back(dest); closedlist[dest.first][dest.second] = true;
+             break;
+        }
+
+    }
     if(!closedlist[dest.first][dest.second]){
         cout << "Not Found!";
         cout << "Want to try again? (y/n)"<<endl;
-        Rectangle tmp(len/2,br/2,len,br);
-        tmp.imprint();
         char c; cin >> c;
         if(c=='y') rrt(input, src, dest, n, m, len, br);
         return;
@@ -163,14 +162,26 @@ void rrt(vector<vector<bool>>& input, pair<int,int> src, pair<int,int> dest, int
     getClick();
     print(parent, src, dest, len, br);
     make_grid(n,m,len,br);
+    cout << "Show explored nodes? (y/n):"<< endl;
+    char c;cin >> c;
+    if(c=='y'){
+        for(auto n:nodes){
+            if(n == src) continue;cout << "Showing nodes"<<endl;
+            imprintLine(n.first*len + len/2, n.second*br+len/2, parent[n.first][n.second].first*len + len/2, parent[n.first][n.second].second*br + br/2);
+        }
+    }
+
     cout << "Try again for the same map? (y/n):"<< endl;
-    char c; cin >> c;
+    cin >> c;
     if(c=='y'){
         get_src_dest(src, dest, len, br);
         Rectangle tmp(len/2,br/2,len,br);
         tmp.imprint();
         rrt(input, src, dest, n, m, len, br);
+        return;
     }
+
+
     return;
 
 }
@@ -191,18 +202,7 @@ int main()
     double len,br;
     len = WINDOW_X/m;
     br = WINDOW_Y/n;
-    /*
-    for (int i = 0; i < n; i++)
-    {
-        row.clear();row.reserve(m);
-        for (int j = 0; j < m; j++)
-        {
-            cin >> b;
-             row.push_back(b);
-        }
-        input[i] = row;
-
-    }*/
+    
     int x, y, i, j;
     for(i=0; i<n; i++){
         for(j=0; j<m; j++){
@@ -210,7 +210,22 @@ int main()
             imprintLine(0, j*br, WINDOW_X, j*br);
         }
     }
-    while(true){
+    cout << "Want to type input? (y/n):"<< endl;
+    char c, temp; cin >> c;
+    if(c=='y'){
+      for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+          cin >> temp;
+          input[i][j] = (temp!='0');
+          if(input[i][j]){
+            Rectangle tmp(i*len + len/2, j*br+ br/2,len,br);
+            tmp.setFill(true);
+            tmp.imprint();
+          }
+        }
+      }
+    }
+    else while(true){
         XEvent event;
         nextEvent(event);
         if(mouseButtonPressEvent(event)){
